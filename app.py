e9d7cae06ff1c060882ca4555ccabf0251aa55f7
frncsko <frncsko09@gmail.com>
2026-09-11 01:01:10 -0400

    Fix indentation for submitted condition in app.py

--- app.py
+++ app.py
@@ -351,7 +351,7 @@ if not st.session_state.autenticado:
         
         
 
-        if submitted:
+        
             if tipo_reserva_pub == "A Domicilio":
                 texto_wsp = f"🏠 NUEVA CITA A DOMICILIO - Cliente: {cli_pub}, Tel: {tel_pub}, Barbero: {barbero_pub}, Servicio: {serv_pub}, Dir: {dir_pub}, Fecha: {fecha_hora_str}"
             else:
