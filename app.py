7d5e0a557f1bcccd3065d4e4ed0c216af3e44e97
frncsko <frncsko09@gmail.com>
2026-09-12 01:40:26 -0400

    Update admin password in app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ st.markdown(
 NUMERO_WHATSAPP_ADMIN = "584125205165"
 
 USUARIOS_VALIDOS = {
-    "admin": "godstime123",
+    "admin": "admin",
     "francisco": "barbero1",
     "jonder": "barbero2",
 }
